efb06392ee9259ffcd985a1af581fb3280e79a22
Peter Bisset <peter.bisset@hotmail.co.uk>
2019-06-30 13:27:13 +0100

    completed

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+import express from 'express';
+import app from express();
+import path from 'path';
+
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
